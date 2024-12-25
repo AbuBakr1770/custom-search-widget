078dbda63155d64760b0d9c576941b85f7c7f32d
abubakar-codecync <abubakar@codecync.com>
2024-12-25 16:42:59 +0500

    with buldles

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/main.tsx", // Path to your main entry file
+      name: "SearchWidget",
+      fileName: "widget",
+      formats: ["iife"], // Immediately Invoked Function Expression
+    },
+  },
 });
