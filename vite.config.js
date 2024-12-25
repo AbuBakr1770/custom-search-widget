49d143e9e297fce7b96fc01a8a3efee04076a176
abubakar-codecync <abubakar@codecync.com>
2024-12-25 15:56:30 +0500

    minor config changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/main.tsx",
+      name: "SearchWidget",
+      fileName: "widget",
+      formats: ["iife"],
+    },
+  },
+});
