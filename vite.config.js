276d2cf37ec4355e68f0182dfd725e2fb56d3d75
abubakar-codecync <abubakar@codecync.com>
2024-12-25 17:10:39 +0500

    polly fill env

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {}, // Polyfill process.env in the browser
+  },
   build: {
     lib: {
       entry: "src/main.jsx", // Path to your main entry file
